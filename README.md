6c1b8814eba7e2cba0fb84ade02e9be3d667d322
Mark Allen <mark@markcallen.com>
2014-07-09 10:38:25 -0700

    Added BasicExample.  Setup build.xml to compile and run.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 sampleSocketIO
 ==============
+
+Sample socket.io java application using https://github.com/Gottox/socket.io-java-client/blob/master/examples/basic/BasicExample.java as a starting point.
+
+Build
+=====
+
+ant
+
+
+Run
+==
+
+ant run
---
+++ src/com/sailmeter/sampleSocketIO/BasicExample.java
@@ -0,0 +1,73 @@
+package com.sailmeter.sampleSocketIO;
+/*
+ * socket.io-java-client Test.java
+ *
+ * Copyright (c) 2012, Enno Boland
+ * socket.io-java-client is a implementation of the socket.io protocol in Java.
+ * 
+ * See LICENSE file for more information
+ */
+import io.socket.IOAcknowledge;
+import io.socket.IOCallback;
+import io.socket.SocketIO;
+import io.socket.SocketIOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class BasicExample implements IOCallback {
+	private SocketIO socket;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			new BasicExample();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public BasicExample() throws Exception {
+		socket = new SocketIO();
+		socket.connect("http://afterguardcloud.poweredbypurple.ca/", this);
+		//socket.connect("http://localhost:8888/", this);
+
+	}
+
+	@Override
+	public void onMessage(JSONObject json, IOAcknowledge ack) {
+		try {
+			System.out.println("Server said:" + json.toString(2));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onMessage(String data, IOAcknowledge ack) {
+		System.out.println("Server said: " + data);
+	}
+
+	@Override
+	public void onError(SocketIOException socketIOException) {
+		System.out.println("an Error occured");
+		socketIOException.printStackTrace();
+	}
+
+	@Override
+	public void onDisconnect() {
+		System.out.println("Connection terminated.");
+	}
+
+	@Override
+	public void onConnect() {
+		System.out.println("Connection established");
+	}
+
+	@Override
+	public void on(String event, IOAcknowledge ack, Object... args) {
+		System.out.println("Server triggered event '" + event + "'");
+	}
+}
